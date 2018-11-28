68d35270020ff09c5f334b25537430ad2dc0058a
Elyas Bahodi <elyasbahodi@gmail.com>
2018-11-28 12:52:58 +0100

    Exercise controller and relevant dependencies on web config and the project

---
+++ RESTfull/Controllers/ExerciseController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTfull.Controllers
+{
+    public class ExerciseController : ApiController
+    {
+        // GET: api/Exercise
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Exercise/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Exercise
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Exercise/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Exercise/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
