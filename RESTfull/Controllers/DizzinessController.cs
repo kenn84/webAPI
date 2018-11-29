6340c8fa2297dd0a7d4d5f663366a1a13f3307c3
kenn84 <kenneth84petersen@hotmail.com>
2018-11-29 09:02:22 +0100

    Dizziness controller done Step controller done

---
+++ RESTfull/Controllers/DizzinessController.cs
@@ -0,0 +1,105 @@
+﻿using RESTfull.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTfull.Controllers
+{
+    public class DizzinessController : ApiController
+    {
+        mHealthDatabaseEntities1 db = new mHealthDatabaseEntities1();
+
+
+        [HttpPost]
+        public HttpResponseMessage AddDizziness(int id, DateTime date, int level, int clientId)
+        {
+
+            try
+            {
+                Dizziness dizz = new Dizziness();
+                dizz.id = id;
+                dizz.date = date;
+                dizz.level = level;
+                dizz.clientId = clientId; 
+                db.Dizzinesses.Add(dizz);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Accepted, "Svimmelhed er gemt");
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.ExpectationFailed, ex.ToString());
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetDizziness(int id)
+        {
+            Dizziness dizz = new Dizziness();
+            try
+            {
+                dizz = db.Dizzinesses.ToList().Where((u) => { return u.id == id; }).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            if (dizz == null)
+            {
+                return Ok("Ingen svimmelhed");
+            }
+            else
+            {
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(dizz);
+                return Json(json);
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateClient(int id, int height, DateTime date, int level, int clientId)
+        {
+            Dizziness dizz = new Dizziness();
+            var entry = db.Entry<Dizziness>(dizz);
+            entry.Entity.id= id;
+            entry.Entity.date = date;
+            entry.Entity.level = level;
+            entry.Entity.clientId = clientId; 
+           
+            entry.State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+            return Request.CreateResponse(HttpStatusCode.Accepted, "Svimmelhed er gemt");
+
+        }
+        [HttpDelete]
+        public HttpResponseMessage DeleteClient(int id)
+        {
+            try
+            {
+                db.Dizzinesses.Remove(db.Dizzinesses.Where((u) => u.id == id).FirstOrDefault());
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Accepted, "Svimmelhed er slettet");
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.ExpectationFailed, ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ RESTfull/Controllers/StepController.cs
@@ -0,0 +1,106 @@
+﻿using RESTfull.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTfull.Controllers
+{
+    public class StepController : ApiController
+    {
+        mHealthDatabaseEntities1 db = new mHealthDatabaseEntities1();
+
+
+        [HttpPost]
+        public HttpResponseMessage AddClient(int id, DateTime date, int count, int clientId)
+        {
+
+            try
+            {
+                Step step = new Step();
+                step.id = id;
+                step.date = date;
+                step.count = count;
+                step.clientId = clientId;
+                db.Steps.Add(step);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Accepted, "Skridt er gemt");
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.ExpectationFailed, ex.ToString());
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetClient(int id)
+        {
+            Step step = new Step();
+            try
+            {
+                step = db.Steps.ToList().Where((u) => { return u.id == id; }).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            if (step == null)
+            {
+                return Ok("Ingen skridt fundet");
+            }
+            else
+            {
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(step);
+                return Json(json);
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateClient(int id, DateTime date, int count, int clientId)
+        {
+            Step step = new Step();
+            var entry = db.Entry<Step>(step);
+            entry.Entity.id= id;
+            entry.Entity.date = date;
+            entry.Entity.count = count;
+            entry.Entity.clientId= clientId;
+        
+
+            entry.State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+            return Request.CreateResponse(HttpStatusCode.Accepted, "Skridt er gemt");
+
+        }
+        [HttpDelete]
+        public HttpResponseMessage DeleteClient(int id)
+        {
+            try
+            {
+                db.Steps.Remove(db.Steps.Where((u) => u.id == id).FirstOrDefault());
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Accepted, "Skridt er slettet");
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.ExpectationFailed, ex.ToString());
+            }
+
+        }
+    }
+}
